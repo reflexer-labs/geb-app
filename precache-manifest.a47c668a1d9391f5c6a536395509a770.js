db402cd90098312467d46087c46762a4a25742bc
heavenchains@gmail.com <mostafa@Mostafas-MacBook-Pro.local>
2020-09-25 23:42:29 +0200

    Updates

---
+++ precache-manifest.a47c668a1d9391f5c6a536395509a770.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00f9c383782b954bd881cfc4792974f",
+    "url": "/reflexer-labs/geb-frontend/index.html"
+  },
+  {
+    "revision": "8cf643f93546573a5bc1",
+    "url": "/reflexer-labs/geb-frontend/static/css/main.46a6af4c.chunk.css"
+  },
+  {
+    "revision": "008327211d5160f38626",
+    "url": "/reflexer-labs/geb-frontend/static/js/2.739ba9da.chunk.js"
+  },
+  {
+    "revision": "6f11fc0c10d5bb196c7e9c8f4578486d",
+    "url": "/reflexer-labs/geb-frontend/static/js/2.739ba9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf643f93546573a5bc1",
+    "url": "/reflexer-labs/geb-frontend/static/js/main.201e25f8.chunk.js"
+  },
+  {
+    "revision": "4f3c52d247d2717fcdee",
+    "url": "/reflexer-labs/geb-frontend/static/js/runtime-main.b7c8900f.js"
+  },
+  {
+    "revision": "646c953ecb7aaf858adc92220ba829b2",
+    "url": "/reflexer-labs/geb-frontend/static/media/arrow.646c953e.svg"
+  },
+  {
+    "revision": "04153b4fa9c20cd50fa1aad960c04e67",
+    "url": "/reflexer-labs/geb-frontend/static/media/brand.04153b4f.svg"
+  },
+  {
+    "revision": "637d5d9bf2f9c22bdd46d501d727232e",
+    "url": "/reflexer-labs/geb-frontend/static/media/dark-arrow.637d5d9b.svg"
+  },
+  {
+    "revision": "a713326e9ed25fc2170b82f1b0e403f4",
+    "url": "/reflexer-labs/geb-frontend/static/media/error.a713326e.svg"
+  },
+  {
+    "revision": "4a6a071acb71888d490db2cf170144a7",
+    "url": "/reflexer-labs/geb-frontend/static/media/reflexer-icon.4a6a071a.svg"
+  },
+  {
+    "revision": "b587924a0c525fcd27141ae7acd1bbcf",
+    "url": "/reflexer-labs/geb-frontend/static/media/uniswap-icon.b587924a.svg"
+  }
+]);
