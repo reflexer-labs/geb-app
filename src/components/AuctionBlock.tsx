9bd1d66eb921fc0ced9dec70e45701a0dcad4d9a
mstfash <m.ashraf.hegab@gmail.com>
2021-01-21 15:35:20 +0200

    design revamp

--- src/components/AuctionBlock.tsx
+++ src/components/AuctionBlock.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { ChevronDown, ChevronUp } from 'react-feather';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { ExternalLinkArrow } from '../GlobalStyle';
 import { useStoreActions } from '../store';
 import { COIN_TICKER } from '../utils/constants';
+import AlertLabel from './AlertLabel';
 import Button from './Button';
 
 const AuctionBlock = () => {
@@ -14,47 +14,91 @@ const AuctionBlock = () => {
   return (
     <Container>
       <Header onClick={() => setCollapse(!collapse)}>
-        Auction ID: 625
-        <Info>
-          Auction Can Be Dealt
-          {collapse ? <ChevronDown size="25" /> : <ChevronUp size="25" />}
-        </Info>
+        <LeftAucInfo>
+          <img src={require('../assets/debt.svg')} alt="debt type auction" />
+          {'Auction #625'}
+        </LeftAucInfo>
+
+        <RightAucInfo>
+          {collapse ? (
+            <InfoContainer>
+              <Info>
+                <InfoCol>
+                  <InfoLabel>FLX OFFERED</InfoLabel>
+                  <InfoValue>{`7.36 ${COIN_TICKER}`}</InfoValue>
+                </InfoCol>
+
+                <InfoCol>
+                  <InfoLabel>RAI BID</InfoLabel>
+                  <InfoValue>{`10,000 ${COIN_TICKER}`}</InfoValue>
+                </InfoCol>
+
+                <InfoCol>
+                  <InfoLabel>ENDS ON</InfoLabel>
+                  <InfoValue>{`12:17, 20 Jan`}</InfoValue>
+                </InfoCol>
+              </Info>
+            </InfoContainer>
+          ) : null}
+          <AlertContainer>
+            <AlertLabel text={'Auction is Ongoing'} type="warning" />
+          </AlertContainer>
+        </RightAucInfo>
       </Header>
       {collapse ? null : (
         <Content>
           <SectionContent>
             <InnerContent>
               <Col>
-                <Label>EVENT TYPE</Label>
-                <Value>Tend</Value>
-                <Label>AUCTION DEADLINE</Label>
-                <Value>12:17, 20 Jan</Value>
+                <InnerCol>
+                  <Label>AUCTION DEADLINE</Label>
+                  <Value>12:17, 20 Jan</Value>
+                </InnerCol>
+              </Col>
+              <Col>
+                <InnerCol>
+                  <Label>FLX OFFERED</Label>
+                  <Value>{`7.36 ${COIN_TICKER}`}</Value>
+                </InnerCol>
+              </Col>
+              <Col>
+                <InnerCol>
+                  <Label>{COIN_TICKER} BID</Label>
+                  <Value>{`10,000 ${COIN_TICKER}`} </Value>
+                </InnerCol>
+              </Col>
+              <Col>
+                <InnerCol>
+                  <Label>FLX PRICE</Label>
+                  <Value>1,357.80 {COIN_TICKER}</Value>
+                </InnerCol>
+              </Col>
+              <Col>
+                <InnerCol>
+                  <Label>EVENT TYPE</Label>
+                  <Value>Tend</Value>
+                </InnerCol>
+              </Col>
+              <Col>
+                <InnerCol>
+                  <Label>SENDER</Label>
+                  <Value>
+                    <Link href="" target="_blank" rel="noopener noreferrer">
+                      0x008Ca...4f6C
+                    </Link>
+                  </Value>
+                </InnerCol>
               </Col>
 
               <Col>
-                <Label>FLX OFFERED</Label>
-                <Value>{`7.36 ${COIN_TICKER}`}</Value>
-                <Label>SENDER</Label>
-                <Value>
-                  <Link href="" target="_blank" rel="noopener noreferrer">
-                    0x008Ca...4f6C
-                  </Link>
-                </Value>
-              </Col>
-
-              <Col>
-                <Label>{COIN_TICKER} BID</Label>
-                <Value>{`10,000 ${COIN_TICKER}`} </Value>
-                <Label>TX</Label>
-                <Value>
-                  <Link href="" rel="noopener noreferrer">
-                    0x23314...9f22
-                  </Link>
-                </Value>
-              </Col>
-              <Col>
-                <Label>FLX PRICE</Label>
-                <Value>1,357.80 {COIN_TICKER}</Value>
+                <InnerCol>
+                  <Label>TX</Label>
+                  <Value>
+                    <Link href="" rel="noopener noreferrer">
+                      0x23314...9f22
+                    </Link>
+                  </Value>
+                </InnerCol>
               </Col>
             </InnerContent>
             <BtnContainer>
@@ -78,7 +122,6 @@ const Container = styled.div`
   border: 1px solid ${(props) => props.theme.colors.border};
   margin-bottom: 15px;
   background: ${(props) => props.theme.colors.background};
-  margin-bottom: 20px;
 `;
 const Header = styled.div`
   font-size: ${(props) => props.theme.font.medium};
@@ -88,16 +131,48 @@ const Header = styled.div`
   align-items: center;
   justify-content: space-between;
   cursor: pointer;
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    flex-direction: column;
+    align-items:flex-start;
+  `}
 `;
 
 const Info = styled.div`
   display: flex;
   align-items: center;
-  font-size: ${(props) => props.theme.font.default};
-  svg {
-    margin-left: 20px;
-  }
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+      flex-direction:column;
+    
+  `}
 `;
+
+const InfoCol = styled.div`
+  font-size: ${(props) => props.theme.font.small};
+  margin-left: 30px;
+
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+      flex: 0 0 100%;
+      min-width:100%;
+      display:flex;
+      align-items:center;
+      justify-content:space-between;
+      margin-left:0;
+      margin-top:5px;
+    
+  `}
+`;
+
+const InfoLabel = styled.div`
+  color: ${(props) => props.theme.colors.secondary};
+  font-size: ${(props) => props.theme.font.extraSmall};
+`;
+const InfoValue = styled.div`
+  margin-top: 3px;
+  color: ${(props) => props.theme.colors.primary};
+  font-weight: normal;
+  font-size: ${(props) => props.theme.font.extraSmall};
+`;
+
 const Content = styled.div`
   padding: 20px;
   border-top: 1px solid ${(props) => props.theme.colors.border};
@@ -108,21 +183,31 @@ const SectionContent = styled.div`
 `;
 
 const InnerContent = styled.div`
-  background: ${(props) => props.theme.colors.foreground};
-  border: 1px solid ${(props) => props.theme.colors.border};
-  padding: 20px 20px 15px 20px;
   display: flex;
-  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+  margin: 0 -10px;
+`;
+
+const InnerCol = styled.div`
+  border-radius: ${(props) => props.theme.global.borderRadius};
+  border: 1px solid ${(props) => props.theme.colors.border};
+  background: ${(props) => props.theme.colors.foreground};
+  text-align: center;
+  height: 100%;
+  padding: 20px;
 `;
 
 const Col = styled.div`
-  flex: 0 0 25%;
+  padding: 0 7.5px;
+  flex-grow: 1;
+  margin-bottom: 15px;
 `;
 
 const Label = styled.div`
   color: ${(props) => props.theme.colors.secondary};
   font-weight: bold;
-  font-size: ${(props) => props.theme.font.extraSmall};
+  font-size: ${(props) => props.theme.font.small};
   margin-top: 15px;
   &:first-child {
     margin-top: 0;
@@ -130,7 +215,7 @@ const Label = styled.div`
 `;
 const Value = styled.div`
   color: #272727;
-  font-size: ${(props) => props.theme.font.extraSmall};
+  font-size: ${(props) => props.theme.font.small};
   margin-top: 5px;
 `;
 
@@ -141,5 +226,46 @@ const Link = styled.a`
 
 const BtnContainer = styled.div`
   text-align: right;
-  margin-top: 15px;
+  margin-top: 10px;
+`;
+
+const LeftAucInfo = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    margin-right: 20px;
+  }
+`;
+
+const RightAucInfo = styled.div`
+  display: flex;
+  align-items: center;
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+      flex: 0 0 100%;
+      min-width:100%;
+      flex-direction:column;
+  `}
+`;
+
+const AlertContainer = styled.div`
+  div {
+    font-size: 13px;
+    margin-left: 80px;
+    ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    margin-left:0;
+    width:100%;
+  `}
+  }
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+      width:100%;
+      margin-top:10px;
+      margin-bottom:10px;
+  `}
+`;
+
+const InfoContainer = styled.div`
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    order:2;
+    min-width:100%;
+  `}
 `;
--- src/containers/Auctions/index.tsx
+++ src/containers/Auctions/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 import AuctionsFAQ from '../../components/AuctionsFAQ';
+import Button from '../../components/Button';
 import GridContainer from '../../components/GridContainer';
 import PageHeader from '../../components/PageHeader';
 import AuctionsList from './AuctionsList';
@@ -8,14 +10,24 @@ import AuctionsList from './AuctionsList';
 const Auctions = () => {
   const { t } = useTranslation();
 
+  const [hide, setHide] = useState(false);
+
+  const handleHideFAQ = () => setHide(!hide);
+
   return (
     <>
       <GridContainer>
-        <PageHeader
-          breadcrumbs={{ '/': t('auctions') }}
-          text={t('auctions_header_text')}
-        />
-        <AuctionsFAQ />
+        <Content>
+          <PageHeader
+            breadcrumbs={{ '/': t('auctions') }}
+            text={t('auctions_header_text')}
+          />
+          {hide ? (
+            <Button text={t('show_faq')} onClick={handleHideFAQ} />
+          ) : null}
+        </Content>
+
+        {hide ? null : <AuctionsFAQ hideFAQ={handleHideFAQ} />}
         <AuctionsList />
       </GridContainer>
     </>
@@ -23,3 +35,17 @@ const Auctions = () => {
 };
 
 export default Auctions;
+
+const Content = styled.div`
+  position: relative;
+  button {
+    position: absolute;
+    top: 15px;
+    right: 0px;
+    min-width: auto;
+    padding: 2px 10px;
+    border-radius: 25px;
+    font-size: 14px;
+    background: linear-gradient(225deg, #4ce096 0%, #78d8ff 100%);
+  }
+`;
