c28390f17cdb176d277b991edfe566eddfed2342
Guillaume Felley <guillaume@felley.io>
2021-01-28 18:49:53 +0100

    Add RPC query and test for liquidation data and safe list

--- src/utils/gebManager/actions.test.ts
+++ src/utils/gebManager/actions.test.ts
@@ -1,127 +1,258 @@
 // tests
-import gebManager from '.';
-import '../../setupTests';
+import gebManager from ".";
+import "../../setupTests";
+import axios from "axios";
+import { GRAPH_API_URLS } from "../constants";
+import { getUserSafesListQuery, liquidationQuery } from "../queries/safe";
+import { BigNumber, FixedNumber } from "ethers";
 
-describe('actions', () => {
-  const address = '0xe94d94eddb2322975d73ca3f2086978e0f2953b1';
-  const safeId = '16';
+// Add custom match type
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      fixedNumberMatch: (other: string) => void;
+      almostEqual: (other: string, maxAbsoluteDeviation: number) => void;
+    }
+  }
+}
 
-  const liquidationResponse = {
-    systemState: {
-      currentRedemptionPrice: {
-        value: expect.any(String),
-      },
-      currentRedemptionRate: {
-        eightHourlyRate: expect.any(String),
-      },
-      globalDebt: expect.any(String),
-      globalDebtCeiling: expect.any(String),
-      perSafeDebtCeiling: expect.any(String),
-    },
-    collateralType: {
-      accumulatedRate: expect.any(String),
-      currentPrice: {
-        liquidationPrice: expect.any(String),
-        safetyPrice: expect.any(String),
-        value: expect.any(String),
-      },
-      debtCeiling: expect.any(String),
-      debtFloor: expect.any(String),
-      liquidationCRatio: expect.any(String),
-      liquidationPenalty: expect.any(String),
-      safetyCRatio: expect.any(String),
-      totalAnnualizedStabilityFee: expect.any(String),
-    },
-  };
+// Add some custom matchers
+expect.extend({
+  // Compare numbers as string regardless of things like trailing/leading zeros
+  // And with rounding to the 28th decimal
+  fixedNumberMatch(received, other: string) {
+    // Round to 28 decimals, the max that the graph supports
+    const round = (fx: FixedNumber) => {
+      // 10**28
+      const factor = FixedNumber.from(
+        "10000000000000000000000000000",
+        fx.format
+      );
+      return fx.mulUnsafe(factor).floor().divUnsafe(factor);
+    };
 
-  describe('FetchLiquidationData', () => {
-    it('fetches liquidation data', () => {
-      const response = gebManager.getLiquidationData();
-      expect(response).toBeTruthy();
-      expect(response).toEqual(liquidationResponse);
+    // Use format 45 decimal
+    const fixReceived = round(FixedNumber.from(received, "fixed256x45"));
+    const fixOther = round(FixedNumber.from(other, "fixed256x45"));
+
+    // Compare the mantissa and the decimal place
+    if (
+      fixReceived.toHexString() === fixOther.toHexString() &&
+      fixReceived.format.decimals === fixOther.format.decimals
+    ) {
+      return { pass: true, message: () => "Good" };
+    } else {
+      return {
+        pass: false,
+        message: () => `Got ${received} not matching ${other}`,
+      };
+    }
+  },
+  // Equality with a specified tolerance
+  almostEqual(received: string, other: string, maxAbsoluteDeviation: number) {
+    const deviation = Math.abs(Number(received) - Number(other));
+    if (deviation <= maxAbsoluteDeviation) {
+      return { pass: true, message: () => "Good" };
+    } else {
+      return {
+        pass: false,
+        message: () =>
+          `Got ${received} not matching ${other} \n Deviation of ${deviation} is too large`,
+      };
+    }
+  },
+});
+
+// Recursively checks that 2 objects have the same keys and number of array element
+const verifyKeys = (objA: any, objB: any, matchArrays = true) => {
+  const keyA = Object.keys(objA).sort();
+  const keyB = Object.keys(objB).sort();
+
+  if (keyA.length !== keyB.length) {
+    fail("Objects don't have the same number of key");
+  }
+
+  for (let i = 0; i < keyA.length; i++) {
+    if (keyA[i] !== keyB[i]) {
+      fail("Key names not matching");
+    }
+
+    if (typeof objA[keyA[i]] !== typeof objB[keyB[i]]) {
+      fail("Type of object not matching");
+    }
+
+    if (Array.isArray(objA[keyA[i]]) && matchArrays) {
+      // Process arrays
+      const arrayA = objA[keyA[i]];
+      const arrayB = objB[keyB[i]];
+      // Make sure that the arrays are of the same length
+      expect(arrayA.length).toEqual(arrayB.length);
+
+      // Check each individual objec within the aeeay
+      for (let j = 0; j < arrayA.length; j++) {
+        verifyKeys(arrayA[j], arrayB[j]);
+      }
+    } else if (typeof objA[keyA[i]] == "object") {
+      verifyKeys(objA[keyA[i]], objB[keyB[i]]);
+    }
+  }
+};
+
+describe("actions", () => {
+  const address = "0xe94d94eddb2322975d73ca3f2086978e0f2953b1";
+  const safeId = "16";
+
+  // const liquidationResponse = {
+  //   systemState: {
+  //     currentRedemptionPrice: {
+  //       value: expect.any(String),
+  //     },
+  //     currentRedemptionRate: {
+  //       eightHourlyRate: expect.any(String),
+  //     },
+  //     globalDebt: expect.any(String),
+  //     globalDebtCeiling: expect.any(String),
+  //     perSafeDebtCeiling: expect.any(String),
+  //   },
+  //   collateralType: {
+  //     accumulatedRate: expect.any(String),
+  //     currentPrice: {
+  //       liquidationPrice: expect.any(String),
+  //       safetyPrice: expect.any(String),
+  //       value: expect.any(String),
+  //     },
+  //     debtCeiling: expect.any(String),
+  //     debtFloor: expect.any(String),
+  //     liquidationCRatio: expect.any(String),
+  //     liquidationPenalty: expect.any(String),
+  //     safetyCRatio: expect.any(String),
+  //     totalAnnualizedStabilityFee: expect.any(String),
+  //   },
+  // };
+
+  describe("FetchLiquidationData", () => {
+    // prettier-ignore
+    it("Data from RPC and GQL should be the same for liquidation data", async () => {
+      const rpcResponse = await gebManager.getLiquidationDataRpc();
+
+      const gqlQuery = `{ ${liquidationQuery} }`;
+      const gqlResponse = (
+        await axios.post(GRAPH_API_URLS[0], JSON.stringify({ query: gqlQuery }))
+      ).data.data;
+
+      expect(rpcResponse).toBeTruthy();
+      expect(gqlResponse).toBeTruthy();
+
+      verifyKeys(rpcResponse, gqlResponse)
+
+      expect(rpcResponse.systemState.currentRedemptionPrice.value).fixedNumberMatch(gqlResponse.systemState.currentRedemptionPrice.value);
+      // Since we're using JS instead of solidity for the exponentiation, an approximation is enough
+      expect(rpcResponse.systemState.currentRedemptionRate.eightHourlyRate).almostEqual(gqlResponse.systemState.currentRedemptionRate.eightHourlyRate, 0.00001)
+      expect(rpcResponse.systemState.globalDebt).fixedNumberMatch(gqlResponse.systemState.globalDebt);
+      expect(rpcResponse.systemState.globalDebtCeiling).fixedNumberMatch(gqlResponse.systemState.globalDebtCeiling)
+      expect(rpcResponse.systemState.perSafeDebtCeiling).fixedNumberMatch(gqlResponse.systemState.perSafeDebtCeiling)
+      expect(rpcResponse.collateralType.accumulatedRate).fixedNumberMatch(gqlResponse.collateralType.accumulatedRate)
+      expect(rpcResponse.collateralType.currentPrice.liquidationPrice).fixedNumberMatch(gqlResponse.collateralType.currentPrice.liquidationPrice)
+      expect(rpcResponse.collateralType.currentPrice.safetyPrice).fixedNumberMatch(gqlResponse.collateralType.currentPrice.safetyPrice)
+      // This value is derive from other value and therefore can have a small deviation
+      expect(rpcResponse.collateralType.currentPrice.value).almostEqual(gqlResponse.collateralType.currentPrice.value, 0.01)
+      expect(rpcResponse.collateralType.debtCeiling).fixedNumberMatch(gqlResponse.collateralType.debtCeiling)
+      expect(rpcResponse.collateralType.debtFloor).fixedNumberMatch(gqlResponse.collateralType.debtFloor)
+      expect(rpcResponse.collateralType.liquidationCRatio).fixedNumberMatch(gqlResponse.collateralType.liquidationCRatio)
+      expect(rpcResponse.collateralType.liquidationPenalty).fixedNumberMatch(gqlResponse.collateralType.liquidationPenalty)
+      expect(rpcResponse.collateralType.safetyCRatio).fixedNumberMatch(gqlResponse.collateralType.safetyCRatio)
+      // Here we're using JS exponentiation again, so get an approximate value 
+      expect(rpcResponse.collateralType.totalAnnualizedStabilityFee).almostEqual(gqlResponse.collateralType.totalAnnualizedStabilityFee, 0.00001)
     });
   });
 
-  describe('FetchUserSafeList', () => {
-    it('fetches a list of user safes', () => {
-      const response = gebManager.getUserSafes({ address });
-      expect(response).toBeTruthy();
-      expect(response).toEqual({
-        safes: [
-          {
-            collateral: expect.any(String),
-            createdAt: expect.any(String),
-            debt: expect.any(String),
-            safeHandler: expect.any(String),
-            safeId: expect.any(String),
-          },
-        ],
-        erc20Balances: [
-          {
-            balance: expect.any(String),
-          },
-        ],
-        ...liquidationResponse,
-      });
+  describe("FetchUserSafeList", () => {
+    it("fetches a list of user safes", async () => {
+      const rpcResponse = await gebManager.getUserSafesRpc({ address });
+      const gqlResponse = (
+        await axios.post(
+          GRAPH_API_URLS[0],
+          JSON.stringify({ query: getUserSafesListQuery(address) })
+        )
+      ).data.data;
+
+      expect(gqlResponse).toBeTruthy();
+      expect(rpcResponse).toBeTruthy();
+
+      verifyKeys(rpcResponse, gqlResponse);
+
+      expect(rpcResponse.erc20Balances[0].balance).fixedNumberMatch(
+        gqlResponse.erc20Balances[0].balance
+      );
+
+      for (let i = 0; i < rpcResponse.safes.length; i++) {
+        let rpcSafe = rpcResponse.safes[i];
+        let gqlSafe = gqlResponse.safes[i];
+
+        expect(rpcSafe.collateral).fixedNumberMatch(gqlSafe.collateral);
+        expect(rpcSafe.debt).fixedNumberMatch(gqlSafe.debt);
+        expect(rpcSafe.safeHandler).toEqual(gqlSafe.safeHandler);
+        expect(rpcSafe.safeId).toEqual(gqlSafe.safeId);
+      }
     });
   });
 
-  describe('FetchSafeById', () => {
-    it('fetches a safe by id', () => {
-      const response = gebManager.getSafeById({ address, safeId });
+  describe("FetchSafeById", () => {
+    it("fetches a safe by id", async () => {
+      const response = await gebManager.getSafeByIdRpc({ address, safeId });
       expect(response).toBeTruthy();
-      expect(response).toEqual({
-        safes: [
-          {
-            collateral: expect.any(String),
-            createdAt: expect.any(String),
-            debt: expect.any(String),
-            internalCollateralBalance: {
-              balance: expect.any(String),
-            },
-            liquidationFixedDiscount: [
-              {
-                createdAt: expect.any(String),
-                createdAtTransaction: expect.any(String),
-                sellAmount: expect.any(String),
-                sellInitialAmount: expect.any(String),
-              },
-            ],
-            modifySAFECollateralization: [
-              {
-                accumulatedRate: expect.any(String),
-                createdAt: expect.any(String),
-                createdAtTransaction: expect.any(String),
-                deltaCollateral: expect.any(String),
-                deltaDebt: expect.any(String),
-              },
-              {
-                accumulatedRate: expect.any(String),
-                createdAt: expect.any(String),
-                createdAtTransaction: expect.any(String),
-                deltaCollateral: expect.any(String),
-                deltaDebt: expect.any(String),
-              },
-            ],
-            safeId: expect.any(String),
-          },
-        ],
-        erc20Balances: [
-          {
-            balance: expect.any(String),
-          },
-        ],
-        userProxies: [
-          {
-            address: expect.any(String),
-            coinAllowance:
-              {
-                amount: expect.any(String),
-              } || expect.any(null),
-          },
-        ],
-        ...liquidationResponse,
-      });
+      // expect(response).toEqual({
+      //   safes: [
+      //     {
+      //       collateral: expect.any(String),
+      //       createdAt: expect.any(String),
+      //       debt: expect.any(String),
+      //       internalCollateralBalance: {
+      //         balance: expect.any(String),
+      //       },
+      //       liquidationFixedDiscount: [
+      //         {
+      //           createdAt: expect.any(String),
+      //           createdAtTransaction: expect.any(String),
+      //           sellAmount: expect.any(String),
+      //           sellInitialAmount: expect.any(String),
+      //         },
+      //       ],
+      //       modifySAFECollateralization: [
+      //         {
+      //           accumulatedRate: expect.any(String),
+      //           createdAt: expect.any(String),
+      //           createdAtTransaction: expect.any(String),
+      //           deltaCollateral: expect.any(String),
+      //           deltaDebt: expect.any(String),
+      //         },
+      //         {
+      //           accumulatedRate: expect.any(String),
+      //           createdAt: expect.any(String),
+      //           createdAtTransaction: expect.any(String),
+      //           deltaCollateral: expect.any(String),
+      //           deltaDebt: expect.any(String),
+      //         },
+      //       ],
+      //       safeId: expect.any(String),
+      //     },
+      //   ],
+      //   erc20Balances: [
+      //     {
+      //       balance: expect.any(String),
+      //     },
+      //   ],
+      //   userProxies: [
+      //     {
+      //       address: expect.any(String),
+      //       coinAllowance:
+      //         {
+      //           amount: expect.any(String),
+      //         } || expect.any(null),
+      //     },
+      //   ],
+      //   ...liquidationResponse,
+      // });
     });
   });
 });
--- src/utils/gebManager/index.ts
+++ src/utils/gebManager/index.ts
@@ -1,10 +1,13 @@
-import { geb } from '../../connectors';
-import { ILiquidationResponse, ISafeQuery, IUserSafeList } from '../interfaces';
+import { BigNumber } from "ethers";
+import { utils } from "geb.js";
+import { geb } from "../../connectors";
 import {
-  liquidationMockedResponse,
-  userSafesMockedResponse,
-  userSingleSafeMockedResponse,
-} from './mocks';
+  ILiquidationResponse,
+  ISafeQuery,
+  ISafeResponse,
+  IUserSafeList,
+} from "../interfaces";
+import { userSafesMockedResponse, userSingleSafeMockedResponse } from "./mocks";
 
 interface UserListConfig {
   address: string;
@@ -15,37 +18,137 @@ interface UserListConfig {
 type SingleSafeConfig = UserListConfig & { safeId: string };
 
 // returns LiquidationData
-const getLiquidationData = (
-  collateralTypeId = 'ETH-A',
-  systemStateTypeId = 'current'
-): ILiquidationResponse => {
-  console.log(geb);
+const getLiquidationDataRpc = async (
+  collateralTypeId = "ETH-A",
+  systemStateTypeId = "current"
+): Promise<ILiquidationResponse> => {
+  if (collateralTypeId !== "ETH-A") {
+    throw Error(`Collateral ${collateralTypeId} not supported`);
+  }
 
-  return liquidationMockedResponse;
+  // Massive multicall query to fetch everything one shot
+  // @ts-ignore Typing only supported up to 7 queries
+  const resp: any = await geb.multiCall([
+    geb.contracts.oracleRelayer.redemptionPrice_readOnly(true), // 0
+    geb.contracts.oracleRelayer.redemptionRate(true), // 1
+    geb.contracts.safeEngine.globalDebt(true), // 2
+    geb.contracts.safeEngine.globalDebtCeiling(true), // 3
+    geb.contracts.safeEngine.safeDebtCeiling(true), // 4
+    geb.contracts.safeEngine.collateralTypes(utils.ETH_A, true), // 5
+    geb.contracts.oracleRelayer.collateralTypes(utils.ETH_A, true), // 6
+    geb.contracts.liquidationEngine.collateralTypes(utils.ETH_A, true), // 7
+    geb.contracts.taxCollector.collateralTypes(utils.ETH_A, true), // 8
+  ]);
+
+  return {
+    systemState: {
+      currentRedemptionPrice: {
+        value: parseRay(resp[0]),
+      },
+      currentRedemptionRate: {
+        // Calculate 8h exponentiation of the redemption rate in JS instead of solidity
+        eightHourlyRate: Math.pow(
+          Number(parseRay(resp[1])),
+          3600 * 8
+        ).toString(),
+      },
+      globalDebt: parseRad(resp[2]),
+      globalDebtCeiling: parseRad(resp[3]),
+      perSafeDebtCeiling: parseWad(resp[4]),
+    },
+    collateralType: {
+      accumulatedRate: parseRay(resp[5].accumulatedRate),
+      currentPrice: {
+        liquidationPrice: parseRay(resp[5].liquidationPrice),
+        safetyPrice: parseRay(resp[5].safetyPrice),
+        // Price not directly available but can be calculated
+        // Price feed price = safetyPrice * safetyCRatio * redemptionPrice
+        value: parseRad(
+          resp[5].safetyPrice
+            .mul(resp[6].safetyCRatio)
+            .mul(resp[0])
+            .div(BigNumber.from(10).pow(36))
+        ),
+      },
+      debtCeiling: parseRad(resp[5].debtCeiling),
+      debtFloor: parseRad(resp[5].debtFloor),
+      liquidationCRatio: parseRay(resp[6].liquidationCRatio),
+      liquidationPenalty: parseWad(resp[7].liquidationPenalty),
+      safetyCRatio: parseRay(resp[6].safetyCRatio),
+      totalAnnualizedStabilityFee: Math.pow(
+        Number(parseRay(resp[8].stabilityFee)),
+        3600 * 24 * 365 // Second per year
+      ).toString(),
+    },
+  };
 };
 
-// returns list of user safes
-const getUserSafes = (config: UserListConfig): IUserSafeList => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { address, proxy_not = null, safeId_not = null } = config;
+// Returns list of user safes
+// This is slow since it's 3 chained requests to a RPC node.
+// TODO: Pass-in the proxy if available so that request 2 can be added to the multicall
+// This could be optimized further with a dedicated contract fetching exactly the needed date
+const getUserSafesRpc = async (
+  config: UserListConfig
+): Promise<IUserSafeList> => {
+  const multiCallRequest = geb.multiCall([
+    geb.contracts.coin.balanceOf(config.address, true), // 0
+    geb.contracts.proxyRegistry.proxies(config.address, true), // 1
+  ]);
 
-  console.log(geb);
+  // Fetch the liq data and the a multicall in parallel
+  const [multiCall, liquidationDataRpc] = await Promise.all([
+    multiCallRequest,
+    getLiquidationDataRpc(),
+  ]);
 
-  return userSafesMockedResponse;
+  const safeDetails = await geb.contracts.getSafes.getSafesAsc(
+    geb.contracts.safeManager.address,
+    multiCall[1]
+  );
+
+  const collateralAndDebtRequest = safeDetails.safes.map((handler) =>
+    geb.contracts.safeEngine.safes(utils.ETH_A, handler, true)
+  );
+
+  // @ts-ignore typing does not support this
+  const collateralAndDebt = await geb.multiCall(collateralAndDebtRequest); //RMV
+
+  let safe: ISafeResponse[] = [];
+  for (let i = 0; i < collateralAndDebt.length; i++) {
+    safe.push({
+      collateral: parseWad(collateralAndDebt[i].lockedCollateral),
+      debt: parseWad(collateralAndDebt[i].generatedDebt),
+      createdAt: "0",
+      safeHandler: safeDetails.safes[i].toLowerCase(),
+      safeId: safeDetails.ids[i].toString(),
+    });
+  }
+
+  return {
+    safes: safe,
+    erc20Balances: [
+      {
+        balance: parseWad(multiCall[0]),
+      },
+    ],
+    ...liquidationDataRpc,
+  };
 };
 
 // returns single user safe by Id
-const getSafeById = (config: SingleSafeConfig): ISafeQuery => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { safeId, address, proxy_not = null, safeId_not = null } = config;
-
-  console.log(geb);
-
-  return userSingleSafeMockedResponse;
+const getSafeByIdRpc = async (
+  config: SingleSafeConfig
+): Promise<ISafeQuery> => {
+  // TODO
+  return await userSingleSafeMockedResponse;
 };
 
 export default {
-  getUserSafes,
-  getSafeById,
-  getLiquidationData,
+  getUserSafesRpc,
+  getSafeByIdRpc,
+  getLiquidationDataRpc,
 };
+
+const parseWad = (val: BigNumber) => utils.wadToFixed(val).toString();
+const parseRay = (val: BigNumber) => utils.rayToFixed(val).toString();
+const parseRad = (val: BigNumber) => utils.radToFixed(val).toString();
