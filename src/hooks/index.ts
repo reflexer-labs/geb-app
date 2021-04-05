0ca8b9a96434e8bb50463a67c46097e9f54e1eb0
mstfash <m.ashraf.hegab@gmail.com>
2021-04-05 19:18:06 +0200

    testing connection

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -8,7 +8,7 @@ import { injected } from '../connectors'
 import { NetworkContextName } from '../utils/constants'
 import { SafeAppConnector } from '../connectors/SafeAppConnector'
 const safeAppConnector = new SafeAppConnector({
-    supportedChainIds: [1, 42],
+    supportedChainIds: [1, 4],
 })
 export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & {
     chainId?: ChainId
