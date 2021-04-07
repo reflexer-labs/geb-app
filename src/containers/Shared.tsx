ccc99176d98402b7eb9f5c592558e734cbf5481a
mstfash <m.ashraf.hegab@gmail.com>
2021-04-07 18:49:57 +0200

    Revert "Revert "Distributions""
    This reverts commit 98a82f060d7e8ea8899e72482e7b7957c75054e2.

--- src/containers/Shared.tsx
+++ src/containers/Shared.tsx
@@ -32,6 +32,8 @@ import AlertLabel from '../components/AlertLabel'
 import useGeb from '../hooks/useGeb'
 import SafeManagerModal from '../components/Modals/SafeManagerModal'
 import { isAddress } from '@ethersproject/address'
+import DistributionsModal from '../components/Modals/DistributionsModal'
+import ClaimPopup from '../components/ClaimPopup'
 
 interface Props {
     children: ReactNode
@@ -99,6 +101,8 @@ const Shared = ({ children, ...rest }: Props) => {
             if (!connectWalletState.ctHash) {
                 connectWalletActions.setStep(2)
                 const { pathname } = history.location
+                await connectWalletActions.fetchProtBalance(account)
+
                 let address = ''
                 if (pathname && pathname !== '/') {
                     const route = pathname.split('/')[1]
@@ -204,7 +208,9 @@ const Shared = ({ children, ...rest }: Props) => {
             <WalletModal />
             <ApplicationUpdater />
             <BalanceUpdater />
+            <ClaimPopup />
             <TransactionUpdater />
+            <DistributionsModal />
             <LoadingModal />
             <IncentivesModal />
             <AuctionsModal />
