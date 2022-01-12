eb3d124bde91465192e6c476ea22a37b687adab0
mstfash <m.ashraf.hegab@gmail.com>
2022-01-12 06:58:36 +0200

    fix provider polls

--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -1,7 +1,18 @@
+import { providers } from 'ethers'
 import { Web3Provider } from '@ethersproject/providers'
 
 export default function getLibrary(provider: any): Web3Provider {
-    const library = new Web3Provider(provider)
-    library.pollingInterval = 15000
+    const library = new providers.Web3Provider(
+        provider,
+        typeof provider.chainId === 'number'
+            ? provider.chainId
+            : typeof provider.chainId === 'string'
+            ? parseInt(provider.chainId)
+            : 'any'
+    )
+    library.detectNetwork().then((network) => {
+        console.log('Connected to ', network.name)
+        library.pollingInterval = 15_000
+    })
     return library
 }
