6d4355125a80d74682a0ad1ca9935aa4cae7bb66
mstfash <m.ashraf.hegab@gmail.com>
2022-04-25 21:37:32 +0200

    fix

--- src/components/Analytics/GoogleTagManager.tsx
+++ src/components/Analytics/GoogleTagManager.tsx
@@ -5,24 +5,22 @@ import { RouteComponentProps } from 'react-router-dom'
 const GoogleTagManager = ({ location: { pathname } }: RouteComponentProps) => {
     return (
         <Helmet>
-            <>
-                {process.env.REACT_APP_GOOGLE_ANALYTICS_ID ? (
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}`}
-                    />
-                ) : null}
-                {process.env.REACT_APP_GOOGLE_ANALYTICS_ID ? (
-                    <script>
-                        {`window.dataLayer = window.dataLayer || [];
+            {process.env.REACT_APP_GOOGLE_ANALYTICS_ID ? (
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}`}
+                />
+            ) : null}
+            {process.env.REACT_APP_GOOGLE_ANALYTICS_ID ? (
+                <script>
+                    {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
       
         gtag('config', '${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}', {'page_path': '${pathname}'});
       `}
-                    </script>
-                ) : null}
-            </>
+                </script>
+            ) : null}
         </Helmet>
     )
 }
