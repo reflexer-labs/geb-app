4d73cc79051df43151ace4ae0dce2c1df358dade
mstfash <mstfash@users.noreply.github.com>
2022-05-14 23:45:50 +0000

    Deploying to gh-pages from  @ a4da518e973d4a635ac96149826040a470f0cbca 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a1381ec3bddef106d89e46bed4839a7.js"
+  "./precache-manifest.cfa7bafa6bb05f169ad65068cd1e9e19.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
---
+++ static/js/7.be814733.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgeb-app"]=this["webpackJsonpgeb-app"]||[]).push([[7],{651:function(p,n){},652:function(p,n){},910:function(p,n){}}]);
+//# sourceMappingURL=7.be814733.chunk.js.map
