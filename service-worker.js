899e6f053b4ebd9f9148e8d0c6c1420d46d2bc20
mstfash <mstfash@users.noreply.github.com>
2022-07-11 10:03:25 +0000

    Deploying to gh-pages from  @ 12b7c7811c7e1c22acd218ef35edf34181349073 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.29170036c84fcfd2d26af8d05037b252.js"
+  "./precache-manifest.e76a16d289783fec22fde612fa392ce6.js"
 );
 
 self.addEventListener('message', (event) => {
