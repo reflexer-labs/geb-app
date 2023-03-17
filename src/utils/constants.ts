1f1456821a8cdf0bc19294d361544496374b9102
mstfash <m.ashraf.hegab@gmail.com>
2023-03-17 23:48:49 +0200

    show all auctions

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -41,9 +41,7 @@ export const WYRE_WORKER = REACT_APP_WYRE_WORKER || ''
 // 'https://api.thegraph.com/subgraphs/name/guifel/kovan_1_3_0'
 export const GRAPH_API_URLS = REACT_APP_GRAPH_API_URLS
     ? REACT_APP_GRAPH_API_URLS.split(',')
-    : [
-          'https://subgraph-kovan.reflexer.finance/subgraphs/name/reflexer-labs/rai',
-      ]
+    : ['https://subgraph.reflexer.finance/subgraphs/name/reflexer-labs/rai']
 
 export const MAILCHIMP_URL = REACT_APP_MAILCHIMP_URL
 
