5334d77a48749496b54ff15e785644cc4ed2ec6f
mstfash <m.ashraf.hegab@gmail.com>
2023-10-05 10:27:38 +0300

    fix alert label typescript issue on build

--- src/components/AlertLabel.tsx
+++ src/components/AlertLabel.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
     type: string
     text: string
     padding?: string
