a0d369eed8320ccd17bd83d25b186fc9afa2d653
mstfash <m.ashraf.hegab@gmail.com>
2023-10-05 11:42:24 +0300

    upgrading react types

--- src/components/AlertLabel.tsx
+++ src/components/AlertLabel.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {
+interface Props {
     type: string
     text: string
     padding?: string
